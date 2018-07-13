1cc720d993ce70a0c7d522910b54d4626a19768a
Huan LI <zixia@zixia.net>
2018-07-14 00:54:08 +0800

    add client

--- src/puppet-client.ts
+++ src/puppet-client.ts
@@ -15,3 +15,48 @@ import {
   Empty,
   Id,
 }                     from '../generated/wechaty-puppet_pb'
+
+
+import {
+  PuppetClient,
+}                 from '../generated/wechaty-puppet_grpc_pb'
+import {
+  Empty,
+}                 from '../generated/wechaty-puppet_pb'
+
+import grpc from 'grpc'
+
+// tslint:disable:no-console
+
+async function main () {
+  const client = new PuppetClient(
+    'localhost:50051',
+    grpc.credentials.createInsecure()
+  )
+
+  const empty = new Empty()
+
+  client.contactList(empty, (err, response) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log('contactList:', response.getIdList().map(pbId => pbId.getId()))
+  })
+
+  client.selfId(empty, (err, response) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log('selfId:', response.getId())
+  })
+
+}
+
+main()
+// .then(process.exit)
+// .catch(e => {
+//   console.error(e)
+//   process.exit(1)
+// })
