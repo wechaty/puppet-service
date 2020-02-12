3aa3d374d14e96c006515f5f4ed3e6f190c4da41
Huan LI <zixia@zixia.net>
2020-02-12 23:18:09 +0800

    fix test

--- src/grpc/puppet-client.spec.ts
+++ src/grpc/puppet-client.spec.ts
@@ -5,6 +5,8 @@ import { test }  from 'tstest'
 import { PuppetHostieGrpc } from './puppet-client'
 
 test('version()', async (t) => {
-  const puppet = new PuppetHostieGrpc()
+  const puppet = new PuppetHostieGrpc({
+    token: 'test',
+  })
   t.ok(puppet.version())
 })
