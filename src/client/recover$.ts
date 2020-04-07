21bf4a25a56784dcf626b6dcf75d6c7c0c36be0a
Huan LI <zixia@zixia.net>
2020-04-07 23:35:07 +0800

    wip rxjs ...

--- src/client/recover$.ts
+++ src/client/recover$.ts
@@ -8,6 +8,7 @@ import {
   merge,
   // Subscription,
   pipe,
+  of,
   // forkJoin,
 }                 from 'rxjs'
 import {
@@ -39,18 +40,18 @@ export const switchSuccess = (status: true | 'pending') => status === true
  * Actions
  */
 export const resetPuppet   = (puppet: Puppet) => () => puppet.emit('reset', { data: 'RxJS recover$' })
-export const dingHeartbeat = (puppet: Puppet) => () => puppet.ding(`recover$()`)  // AED: Automated External Defibrillator
+export const dingHeartbeat = (puppet: Puppet) => () => puppet.ding(`recover$() AED`)  // AED: Automated External Defibrillator
 
 /**
  * Pipes
  */
-export const heartbeatDing = (puppet: Puppet) => () => pipe(
+export const heartbeatDing = (puppet: Puppet) => pipe(
   tap(_ => log.verbose('Puppet', 'recover$() heartbeatDing()')),
   debounce(() => interval(15 * 1000)),
   tap(dingHeartbeat(puppet)),
 )
 
-export const heartbeatReset = (puppet: Puppet) => () => pipe(
+export const heartbeatReset = (puppet: Puppet) => pipe(
   tap(_ => log.verbose('Puppet', 'recover$() heartbeatReset()')),
   debounce(_ => interval(60 * 1000)),
   mergeMap(_ => interval(60 * 1000).pipe(
@@ -59,9 +60,10 @@ export const heartbeatReset = (puppet: Puppet) => () => pipe(
   )),
 )
 
-export const heartbeatPipe = (puppet: Puppet) => mergeMap(_ => merge(
-  mergeMap(_ => heartbeatDing(puppet)),
-  mergeMap(_ => heartbeatReset(puppet)),
+// ECG: Electro Cardio Graph}
+export const heartbeatECG = (puppet: Puppet) => mergeMap(_ => merge(
+  of(_).pipe(heartbeatDing(puppet)),
+  of(_).pipe(heartbeatReset(puppet)),
 ))
 
 /**
@@ -74,7 +76,7 @@ export function recover$ (puppet: Puppet) {
     mergeMap(_ => heartbeat$(puppet).pipe(
       startWith(undefined), // trigger the throttle stream at start
       tap(payload => log.verbose('Puppet', 'recover$() heartbeat: %s', JSON.stringify(payload))),
-      heartbeatPipe(puppet),
+      heartbeatECG(puppet),
       takeUntil(switchOff$(puppet)),
     )),
   )
