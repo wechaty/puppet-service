22ac3ef19c3dec3c760c791e96fc4b2ba2f11741
Jun Tsai <jcai@ganshane.com>
2020-10-03 21:09:12 +0800

    Add grpc.default_authority option for authorization

--- src/client/puppet-hostie.ts
+++ src/client/puppet-hostie.ts
@@ -222,10 +222,11 @@ export class PuppetHostie extends Puppet {
       endpoint = hostieIpResult.ip + ':' + hostieIpResult.port
     }
 
+    const clientOptions = Object.assign({},GRPC_LIMITATION,{'grpc.default_authority':this.options.token})
     this.grpcClient = new PuppetClient(
       endpoint, // 'localhost:50051',
       grpc.credentials.createInsecure(),
-      GRPC_LIMITATION,
+      clientOptions
     )
   }
 
