d72f52e6e621272c361fa5c84f319bac84cd0c1a
Huan LI (李卓桓) <zixia@zixia.net>
2020-10-26 17:04:22 +0800

    Restore gRPC limitation for protobuf data size to the default 4MB (#86)

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,9 @@ export { log } from 'wechaty-puppet'
 export { VERSION } from './version'
 
 export const GRPC_LIMITATION = {
-  'grpc.max_receive_message_length': 1024 * 1024 * 150,
-  'grpc.max_send_message_length': 1024 * 1024 * 150,
+  // https://github.com/wechaty/wechaty-puppet-hostie/issues/86
+  // 'grpc.max_receive_message_length': 1024 * 1024 * 150,
+  // 'grpc.max_send_message_length': 1024 * 1024 * 150,
 }
 
 export const WECHATY_PUPPET_HOSTIE_TOKEN    = process.env.WECHATY_PUPPET_HOSTIE_TOKEN
