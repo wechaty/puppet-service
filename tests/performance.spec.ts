481b8cd274cca06ae36ae7f7645d4948a222d35b
Huan <zixia@zixia.net>
2021-05-30 02:57:13 +0800

    upgrade mock

--- tests/performance.spec.ts
+++ tests/performance.spec.ts
@@ -24,7 +24,6 @@ import {
   log,
   ContactGender,
   ContactType,
-  throwUnsupportedError,
 }                               from 'wechaty-puppet'
 
 import {
@@ -61,15 +60,6 @@ class PuppetTest extends PuppetMock {
     })
 
     return rawPayload
-  }
-
-  /**
-   * Huan(202105): should be removed
-   *  after PuppetMock updated to the latest version
-   * of Puppet Abstraction
-   */
-  conversationRead (_: string): Promise<void> {
-    throwUnsupportedError('not implemented. See https://github.com/wechaty/wechaty-puppet/pull/132')
   }
 
 }
