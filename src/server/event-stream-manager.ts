6a9832995ae617a67b4e5835d1fd53bac61c5635
Huan (李卓桓) <zixia@zixia.net>
2021-11-19 19:14:10 +0800

    use RxJS Marble testing for recover$ (#184)
    * use RxJS Marble testing for recover$
    
    * add marble tests, decouple recover logic to epic
    
    * cleanup
    
    * 1.7.4
    
    * add marble rxjs unit testing
    
    * 1.7.5
    
    * unit testing passed!
    
    * 1.7.6
    
    * v1.9
    
    * 1.9.1
    
    * code clean
    
    * 1.9.2

--- src/server/event-stream-manager.ts
+++ src/server/event-stream-manager.ts
@@ -1,3 +1,22 @@
+/**
+ *   Wechaty Open Source Software - https://github.com/wechaty
+ *
+ *   @copyright 2016 Huan LI (李卓桓) <https://github.com/huan>, and
+ *                   Wechaty Contributors <https://github.com/wechaty>.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
 import {
   puppet as grpcPuppet,
   grpc,
@@ -62,7 +81,7 @@ class EventStreamManager {
     /**
       * We emit the login event if current the puppet is logged in.
       */
-    if (this.puppet.logonoff()) {
+    if (this.puppet.isLoggedIn) {
       log.verbose('EventStreamManager', 'start() puppet is logged in, emit a login event for downstream')
 
       const payload = {
